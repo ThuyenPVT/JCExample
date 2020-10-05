cb0dd66e1fa0973250c94e346fd52faa13bc83f0
ThuyenPVT <pham.van.thuyen@jobchat.vn>
2020-10-05 20:55:02 +0700

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import 'package:navigator/Widgets/BottomNavigationBarExample.dart';
+import 'package:navigator/screen/bottom_navigation.dart';
 
 // void main() {
 //   runApp(MaterialApp(
